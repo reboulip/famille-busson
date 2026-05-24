2e8c3646f066463072bf054cceead428d5849f67
Romain Reboulleau <58889158+reboulip@users.noreply.github.com>
2026-05-24 15:51:49 +0200

    feat: split chalet presences into past/current/future sections (#3)

--- annuaire/tests/test_views_chalets.py
+++ annuaire/tests/test_views_chalets.py
@@ -55,9 +55,33 @@ def test_chalet_detail_404_on_invalid_pk(auth_client):
 
 
 @pytest.mark.django_db
-def test_chalet_detail_context_has_presences(auth_client, chalet, presence):
+def test_chalet_detail_context_has_future_presences(auth_client, chalet, presence):
     response = auth_client.get(reverse("chalet-detail", kwargs={"pk": chalet.pk}))
-    assert presence in response.context["presences"]
+    assert presence in response.context["future_presences"]
+
+
+@pytest.mark.django_db
+def test_chalet_detail_context_has_past_presence(auth_client, chalet, person):
+    past = PresencePSV.objects.create(
+        person=person, chalet=chalet,
+        start_date=datetime.date(2025, 1, 1),
+        end_date=datetime.date(2025, 1, 14),
+    )
+    response = auth_client.get(reverse("chalet-detail", kwargs={"pk": chalet.pk}))
+    assert past in response.context["past_presences"]
+
+
+@pytest.mark.django_db
+def test_chalet_detail_context_has_current_presence(auth_client, chalet, person):
+    import datetime as dt
+    today = dt.date.today()
+    current = PresencePSV.objects.create(
+        person=person, chalet=chalet,
+        start_date=today - dt.timedelta(days=1),
+        end_date=today + dt.timedelta(days=1),
+    )
+    response = auth_client.get(reverse("chalet-detail", kwargs={"pk": chalet.pk}))
+    assert current in response.context["current_presences"]
 
 
 @pytest.mark.django_db
